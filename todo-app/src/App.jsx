618e50e26ebcde90ee64f73498e47cc1414cda8d
Omermymon <omermymon17@gmail.com>
2025-05-22 12:47:15 +0200

    query debug

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { getTodos, postTodo, editTodo } from "./utils";
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { getTodos, postTodo, editTodo, eraseTodo } from "./utils";
 
 function App() {
-  const quryClient = useQueryClient();
+  const queryClient = useQueryClient();
   const [task, setTask] = useState("");
   const [edittask, setEditTask] = useState("");
   const [editnum, setEditNum] = useState("");
@@ -16,15 +21,29 @@ function App() {
     isError,
     isLoading,
     error,
-  } = useQuery({ queryKey: ["todos"], queryFn: getTodos });
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getTodos,
+    refetchInterval: 10000,
+  });
+
   const addMutate = useMutation({
     mutationFn: postTodo,
-    onSuccess: () => quryClient.invalidateQueries(["todos"]),
+    onSuccess: () => queryClient.invalidateQueries(["todos"]),
   });
   const editMutation = useMutation({
     mutationFn: editTodo,
-    onSuccess: () => quryClient.invalidateQueries(["todos"]),
+    onSuccess: () => queryClient.invalidateQueries(["todos"]),
   });
+
+  const deleteMutation = useMutation({
+    mutationFn: eraseTodo,
+    onSuccess: () => queryClient.invalidateQueries(["todos"]),
+    onError: (error) => {
+      console.error("Delete error", error);
+    },
+  });
+
   function addTodos() {
     addMutate.mutate({ task: task });
     setTask("");
@@ -33,6 +52,11 @@ function App() {
     const idNum = parseInt(id);
     editMutation.mutate({ id: idNum, task });
     setEditTask("");
+  }
+
+  function deleteTodo(id) {
+    const idNum = parseInt(id);
+    deleteMutation.mutate(idNum);
   }
 
   if (isLoading) return <p>Loading todos...</p>;
@@ -49,17 +73,21 @@ function App() {
       {todos.map((todo) => (
         <div key={todo.id} className="divv">
           <p>{todo.task}</p>
-          {editnum!==todo.id?
-          <button onClick={() => setEditNum(todo.id)}>edit task</button>:
-          <div>
-          <input value={edittask} onChange={(e) => setEditTask(e.target.value)}></input>
-          <button onClick={() => changeTodo(todo.id, edittask)}>
-            apply changes
-          </button>
-          <button onClick={() => setEditNum("")}>
-            cancel changes
-          </button>
-          </div>}
+          {editnum !== todo.id ? (
+            <button onClick={() => setEditNum(todo.id)}>edit task</button>
+          ) : (
+            <div>
+              <input
+                value={edittask}
+                onChange={(e) => setEditTask(e.target.value)}
+              ></input>
+              <button onClick={() => changeTodo(todo.id, edittask)}>
+                apply changes
+              </button>
+              <button onClick={() => setEditNum("")}>cancel changes</button>
+            </div>
+          )}
+          <button onClick={() => deleteTodo(todo.id)}>delete</button>
         </div>
       ))}
     </>
@@ -67,4 +95,3 @@ function App() {
 }
 
 export default App;
-
--- todo-app/src/utils.js
+++ todo-app/src/utils.js
@@ -1,36 +1,50 @@
 const GET_URL = "http://127.0.0.1:5000/todos";
 const SET_URL = "http://127.0.0.1:5000/todos";
 const EDIT_URL = "http://127.0.0.1:5000/todos";
-async function getTodos(){
-    const response = await fetch(GET_URL);
-    if (!response.ok){
-        throw new Error("bad fetch");
-    }
-    return response.json()
-};
+const DELETE_URL = "http://127.0.0.1:5000/todos";
 
-async function postTodo(todo){
-    const response = await fetch(SET_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(todo)
-      });
-    if (!response.ok){
-        throw new Error("bad Post");
-    }
-    return response.json();
-};
+async function getTodos() {
+  const response = await fetch(GET_URL);
+  if (!response.ok) {
+    throw new Error("bad fetch");
+  }
+  return response.json();
+}
 
-async function editTodo({ id, task }){
-    const response = await fetch(EDIT_URL+`/${id}`,{method:"PUT", headers: {
-        "Content-Type": "application/json"
-      },body: JSON.stringify({task:task})})
-    if (!response.ok){
-        throw new Error("bad Post");
-    }
-    return response.json();
-};
+async function postTodo(todo) {
+  const response = await fetch(SET_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  });
+  if (!response.ok) {
+    throw new Error("bad Post");
+  }
+  return response.json();
+}
 
-export {postTodo,editTodo,getTodos};
+async function editTodo({ id, task }) {
+  const response = await fetch(EDIT_URL + `/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ task: task }),
+  });
+  if (!response.ok) {
+    throw new Error("bad Post");
+  }
+  return response.json();
+}
+
+async function eraseTodo(id) {
+  const response = await fetch(DELETE_URL + `/${id}`, { method: "DELETE" });
+  if (!response.ok) {
+    return new Error("failed deleting");
+  }
+  return true;
+}
+
+export { postTodo, editTodo, getTodos, eraseTodo };
